51ecc1dbdb618a38f8bdd1dbcb31e9b046f20c15
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-17 20:56:57 -0500

    Created functions for displaying current weather and UV index

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,61 @@
-$(document).ready(function() {
-    // DOM VARIABLES
+$(document).ready(function () {
+  // DOM VARIABLES
 
-    // JS VARIABLES
+  // JS VARIABLES
+  var APIKey = "c120eb4512edae3031f1a647615f6499";
+  var city = "Atlanta";
+  var currentLat;
+  var currentLong;
 
-    // FUNCTION DEFINITIONS
+  // FUNCTION DEFINITIONS
+  // Initialize the page
 
-    // FUNCTION CALLS
+  // Display the current weather
+  function displayCurrentWeather() {
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=" +
+      APIKey;
 
-    // EVENT HANDLERS
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      // Print current weather data
+      $("#city-name").text(response.name);
+      $("#temperature").text(response.main.temp + "°F");
+      $("#humidity").text(response.main.humidity);
+      $("#wind-speed").text(
+        response.wind.speed + " mph, Direction: " + response.wind.deg + "°"
+      );
+      getUVIndex(response.coord.lat, response.coord.lon);
+    });
+  }
 
+  // Get and set the UV index
+  function getUVIndex(lat, lon) {
+    var queryURL =
+      "http://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=" +
+      APIKey;
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(queryURL);
+      $("#uv-index").text(response.value);
+    });
+  }
+
+  // FUNCTION CALLS
+  displayCurrentWeather();
+
+  // EVENT HANDLERS
 });
 
 // GIVEN a weather dashboard with form inputs
@@ -17,10 +64,10 @@ $(document).ready(function() {
 //      -Navbar
 //      -Sidebar with two row (search for city/ list of cities)
 //      -Main with two rows (current weather for displayed city/ five day forecast)
-//      
+//
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-//      
+//
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 //
