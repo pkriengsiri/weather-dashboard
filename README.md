325c069588c5cdb5771219d73f35e4a0d8deceae
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-17 22:04:12 -0500

    Added placeholder layout for HTML

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # weather-dashboard
 A weather dashboard that displays the weather outlook for multiple cities using the OpenWeather API
+
+Twemoji for favicon https://twemoji.twitter.com/
+ 
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -62,11 +62,23 @@ $(document).ready(function () {
 //      Start with with the weather dashboard display loaded on html
 //      Display has:
 //      -Navbar
-//      -Sidebar with two row (search for city/ list of cities)
-//      -Main with two rows (current weather for displayed city/ five day forecast)
+//      -Two columns
+//          -Sidebar with two row (search for city/ list of cities)
+//          -Main with two rows (current weather for displayed city/ five day forecast)
 //
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
+//      Create a function to handle the search
+//          Create and id for the search field and search button
+//          Create and event listener for form submission (on the form or the button??)
+//          Create a callback function for the event listener
+//          Prevent default submit behavior
+//          Get the text from the input field
+//          Store the text in an array of previously searched cities
+//          Store the text in localStorage as the last searched city
+//          
+//          
+//      
 //
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
@@ -76,6 +88,9 @@ $(document).ready(function () {
 //
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+//      Create a function to display the five-day forecast
+//          Generate the query URL
+//          Get the object values for each forecast block
 //
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
