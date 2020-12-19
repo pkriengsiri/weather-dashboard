cdbbda2dddc92456a0e91e234a5713e5e1c89a08
Pete Kriengsiri <pkriengsiri@gmail.com>
2020-12-18 22:19:41 -0500

    Modified page styling before moving hardcoded html to jQuery generated elements.  Added pseudocode to script.js

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (response) {
       // Print current weather data
+      console.log(queryURL);
       $("#city-name").text(response.name);
       $("#temperature").text(response.main.temp + "°F");
       $("#humidity").text(response.main.humidity);
@@ -30,6 +31,7 @@ $(document).ready(function () {
         response.wind.speed + " mph, Direction: " + response.wind.deg + "°"
       );
       getUVIndex(response.coord.lat, response.coord.lon);
+      displayForecast(response.coord.lat, response.coord.lon);
     });
   }
 
@@ -50,6 +52,25 @@ $(document).ready(function () {
       console.log(queryURL);
       $("#uv-index").text(response.value);
     });
+  }
+
+  function displayForecast(lat, lon) {
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&exclude=current,minutely,hourly,alerts&units=imperial&appid=" +
+      APIKey;
+      
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        console.log(queryURL);
+        console.log(response);
+      });
+
   }
 
   // FUNCTION CALLS
@@ -76,9 +97,9 @@ $(document).ready(function () {
 //          Get the text from the input field
 //          Store the text in an array of previously searched cities
 //          Store the text in localStorage as the last searched city
-//          
-//          
-//      
+//
+//
+//
 //
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
@@ -94,6 +115,22 @@ $(document).ready(function () {
 //
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-//
+//      Create a text area and button
+//      Take input from text area and store in array for searched cities
+//      Check to make sure item is not already in array (toLowerCase!!!)
+//      Call display weather with user input as parameter
+//      Create new button for search history with user input as the text
+//      Prepend the button    
+//      For search list:
+//          Create class for buttons
+//          Create an event listener to look for a click on these buttons
+//          Call display weather function with the city
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
+//      Array of cities are stored as a single key/ value in local storage
+//      When page is loaded, if there's nothing in local storage set
+//      ex: var storedCities = JSON.parse(localStorage.getItem("something")) || [];
+//      Call function to create the buttons:
+//          Check the length of the array and remove items if limit reached
+//          .shift() to remove first item, .push() adds to end, .pop() removes last element, unshift() adds to the beginning
+//          
